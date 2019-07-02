a4d9289342da2caa92f927d43600fdae52a79411
unknown <chrisocares>
2019-07-02 13:18:00 -0500

    ADDED : Build project

---
+++ src/main/java/pe/intercorp/retail/clientes/IntercorpRetailApplication.java
@@ -0,0 +1,13 @@
+package pe.intercorp.retail.clientes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntercorpRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntercorpRetailApplication.class, args);
+	}
+
+}
